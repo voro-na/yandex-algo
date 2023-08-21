f31af6434f164109e1c9d74731303bbd36bbbdb4
voro-na <voropaevaanadya@gmail.com>
2023-08-21 18:47:50 +0300

    selection_sort

---
+++ selection_sort.js
@@ -0,0 +1,24 @@
+let sort = (arr) =>{
+    let answer = arr 
+    let len = arr.length
+
+    for(let i = 0; i < len; i ++){
+
+        let min = i
+
+        for(let j = i + 1; j < len; j ++){
+            if (arr[min] > arr[j]){
+                min = j
+            }
+        }
+        if (min !== i){
+            let temp = arr[i]
+            arr[i] = arr[min]
+            arr[min] = temp
+        }
+    }
+
+    return answer
+}
+
+//console.log(sort([5,4, 1, 100, -100, 0]))
